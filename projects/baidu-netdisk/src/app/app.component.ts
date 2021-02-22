a3248473bf82018d7e7e74c14c84f40b4042953f
simplessor <1003659191@qq.com>
2021-02-22 12:20:38 +0800

    dev(bd): basic structure

--- projects/baidu-netdisk/src/app/app.component.ts
+++ projects/baidu-netdisk/src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = '百度云盘网页版';
+  copyrightDate:number = 2021
+  author:string = "Simplessor"
+  ICP:string = "皖ICP备20010938号"
 }
