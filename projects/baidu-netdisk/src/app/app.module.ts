3073713e4557ec90809409352b8af8f832a569ca
simplessor <1003659191@qq.com>
2021-02-22 13:09:31 +0800

    refactor(bd): fold app components' files

--- projects/baidu-netdisk/src/app/app.module.ts
+++ projects/baidu-netdisk/src/app/app.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from 'projects/share/src/main';
+import { AppComponent } from './app/app.component';
 
 @NgModule({
   declarations: [
